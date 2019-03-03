c7f554d585bd7bfb83593a957dc12b51c747679d
Ivan Odintsov <ivan@odintsov.me>
2019-03-03 19:33:42 +0300

    service/make chat active on create

--- src/api/service/Chat/mutationCreateChat.js
+++ src/api/service/Chat/mutationCreateChat.js
@@ -3,7 +3,7 @@ import { Chat } from '../../../models';
 const createChat = async (_, { input }) => {
   return await Chat.findOneAndUpdate(
     { id: input.id },
-    input,
+    { ...input, is_active: true },
     { new: true, upsert: true, setDefaultsOnInsert: true }
   );
 };
