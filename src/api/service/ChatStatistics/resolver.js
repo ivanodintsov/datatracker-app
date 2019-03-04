d6c36544236e3360488e62f8e06e13c520750cb4
Ivan Odintsov <ivan@odintsov.me>
2019-03-04 04:03:09 +0300

    add createDailyStatistics mutation

--- src/api/service/ChatStatistics/resolver.js
+++ src/api/service/ChatStatistics/resolver.js
@@ -1,9 +1,11 @@
 import dailyChatStatistics from './mutationDailyChatStatistics';
 import createPreviousStatistics from './mutationPreviousChatStatistics';
+import createDailyStatistics from './mutationCreateDailyStatistics';
 
 export const resolver = {
   Mutation: {
     dailyChatStatistics,
     createPreviousStatistics,
+    createDailyStatistics,
   }
 };
