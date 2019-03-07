f84e3756d4718d23f9c9c7d617ccc80aa3f551b2
Ivan Odintsov <ivan@odintsov.me>
2019-03-07 03:34:13 +0300

    refactor with async

--- src/api/service/ChatStatistics/mutationPreviousChatStatistics.js
+++ src/api/service/ChatStatistics/mutationPreviousChatStatistics.js
@@ -4,6 +4,8 @@ import { baseStatisticsKeys, baseStatisticsDefault } from '../../../models/Stati
 import { divideObject, sumObjByKey } from '../../../helpers/object';
 import { Chat, ChatDailyStatistics } from '../../../models';
 import { concatPropName } from '../../../helpers/object';
+import async from 'async';
+import asyncPromisified from '../../../helpers/async';
 
 const calculateHoursAvg = R.pipe(
   R.values,
@@ -15,39 +17,63 @@ const calculateHoursAvg = R.pipe(
 );
 
 const createProjectionFromArray = R.reduceRight(R.assoc(R.__, 1), {});
-const createYesterdayProjection = createProjectionFromArray([...baseStatisticsKeys, 'hours']);
-const incrementYesterday = async (chatId, chatDailyDocument) => {
-  const yestardayDayStatictics = R.pipe(
+const createYesterdayProjection = createProjectionFromArray([...baseStatisticsKeys, 'hours', 'chat', 'date']);
+const incrementYesterday = async (chatDailyDocument) => {
+  const yesterdayDayStatistics = R.pipe(
     R.pick(baseStatisticsKeys),
     concatPropName('avg_statistics_yesterday.')
-  )(chatDailyDocument.toJSON());
+  )(chatDailyDocument);
 
-  await Chat.updateOne({ id: chatId }, {
-    $inc: {
-      active_days: 1,
-      ...yestardayDayStatictics,
+  await Chat.updateOne(
+    { id: chatDailyDocument.chat },
+    {
+      $inc: {
+        active_days: 1,
+        ...yesterdayDayStatistics,
+      },
+    }
+  );
+};
+
+const createPreviousStatisticsSingle = async chatDailyYesterday => {
+  const hours = R.path([ 'hours' ], chatDailyYesterday);
+  const avgStatistics = calculateHoursAvg(hours);
+  
+  const update = {
+    $set: {
+      day_avg: avgStatistics,
+      is_processed: true,
+    }
+  };
+
+  await ChatDailyStatistics.updateOne(
+    {
+      chat: chatDailyYesterday.chat,
+      date: chatDailyYesterday.date,
     },
-  });
+    update
+  );
+
+  await incrementYesterday(chatDailyYesterday);
 };
 const createPreviousStatistics = async (_, { id: chatId, date }) => {
   try {
-    const yesterday = moment(date).startOf('day').subtract(1, 'days').toDate();
+    const yesterday = moment(date)
+      .startOf('day')
+      .subtract(1, 'days')
+      .toDate();
+
     const chatDailyYesterdayList = await ChatDailyStatistics.find(
       { chat: chatId, date: { $lte: yesterday }, is_processed: false },
-      createYesterdayProjection
+      createYesterdayProjection,
+      { lean: true },
+    )
+    .exec();
+
+    await asyncPromisified.eachLimit(
+      chatDailyYesterdayList, 1,
+      async.asyncify(createPreviousStatisticsSingle)
     );
-
-    for (let i = 0; i < chatDailyYesterdayList.length; i++) {
-      const chatDailyYesterday = chatDailyYesterdayList[i];
-      const hours = R.path([ 'hours' ], chatDailyYesterday);
-      const avgStatistics = calculateHoursAvg(hours);
-
-      chatDailyYesterday.set('day_avg', avgStatistics);
-      chatDailyYesterday.set('is_processed', true);
-
-      await chatDailyYesterday.save();
-      await incrementYesterday(chatId, chatDailyYesterday);
-    }
   } catch (err) {
     console.log(err);
     return;
