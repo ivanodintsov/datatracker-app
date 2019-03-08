6505e71ebe8ed10233f3ceeb50999acf2b3b5e7a
Ivan Odintsov <ivan@odintsov.me>
2019-03-09 00:22:40 +0300

    add averages values to Chat query

--- src/api/client/Chat/queryChat.js
+++ src/api/client/Chat/queryChat.js
@@ -2,12 +2,30 @@ import R from 'ramda';
 import { Chat } from '../../../models';
 import { ResourceError } from '../errors';
 
+const getActiveDays = R.pipe(
+  R.propOr(1, 'active_days'),
+  R.or(R.__, 1),
+);
+
+const toFixed = num => +Number.parseFloat(num).toFixed(2);
+
 const chat = async (_, { id }) => {
-  const chat = await Chat.findOne({ id });
+  const chat = await Chat.findOne({ id }, null, { lean: true }).exec();
 
   if (R.isNil(chat)) {
     throw new ResourceError();
   }
+
+  const activeDays = getActiveDays(chat);
+  const avgTotal = R.pathOr(0, ['avg_statistics_yesterday', 'total'], chat);
+
+  chat.avg_hourly = {
+    total: toFixed(avgTotal / activeDays / 24),
+  };
+
+  chat.avg_daily = {
+    total: toFixed(avgTotal / activeDays),
+  };
 
   return chat;
 };
