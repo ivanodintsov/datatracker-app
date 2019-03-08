ea3b75c6124f1687eb9b13b106559e40778190dd
Ivan Odintsov <ivan@odintsov.me>
2019-03-08 23:16:24 +0300

    add updating members_count in Chat
    update members_count in ChatDailyStatistics if members_count exists in input

--- src/api/service/Chat/mutationUpdateChat.js
+++ src/api/service/Chat/mutationUpdateChat.js
@@ -1,7 +1,21 @@
-import { Chat } from '../../../models';
+import R from 'ramda';
+import { Chat, ChatDailyStatistics } from '../../../models';
+import { yesterdayDate } from '../../../helpers/moment';
 
 const updateChat = async (_, { id, input }) => {
-  return await Chat.findOneAndUpdate({ id }, input, { new: true });
+  await Chat.updateOne({ id }, input, { new: true, lean: true }).exec();
+
+  let dailyUpdate = {};
+  const membersCount = R.prop('members_count', input);
+
+  if (membersCount) {
+    dailyUpdate = R.assocPath(['$set', 'members_count'], membersCount, dailyUpdate);
+  }
+
+  if (!R.isEmpty(dailyUpdate)) {
+    const yesterday = yesterdayDate();
+    await ChatDailyStatistics.updateOne({ chat: id, date: yesterday }, dailyUpdate, { lean: true }).exec();
+  }
 };
 
 export default updateChat;
