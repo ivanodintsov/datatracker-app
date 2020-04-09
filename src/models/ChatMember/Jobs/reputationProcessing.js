d6806cffc4d956dda8141233e4bfeaa3fc60b83e
Ivan Odyntsov <iwan.odintsov@gmail.com>
2020-04-09 05:04:38 +0300

    add reputation queues

--- src/models/ChatMember/Jobs/reputationProcessing.js
+++ src/models/ChatMember/Jobs/reputationProcessing.js
@@ -1,6 +1,9 @@
 import Queue from 'bull';
 import { queues } from '../../../config';
 import reputationService from '../Services/reputationService';
+import ChatMember from '../ChatMember';
+import ChatDailyReputation from '../../ChatDailyStatistics/Jobs/reputationProcessing';
+import ChatMembersStatsReputation from '../../ChatMembersStats/Jobs/reputationProcessing';
 
 const reputationQueue = new Queue('app_reputation', queues);
 
@@ -9,7 +12,20 @@ const NAME = 'reputationProcessing';
 reputationQueue.process(NAME, 5, async function (job) {
   const message = job.data.message;
 
-  await reputationService(message);
+  const reputation = await reputationService(message);
+
+  if (!reputation) {
+    return;
+  }
+
+  await ChatMember.changeReputation({
+    chat: reputation.replyedMessage.chat,
+    user: reputation.replyedMessage.from,
+    type: reputation.reaction.type,
+  });
+
+  ChatDailyReputation.addToQueue({ reputation, message });
+  ChatMembersStatsReputation.addToQueue({ reputation, message });
 
   return Promise.resolve();
 });
