145b4932a921e9dc255c037da5a1fbf4dac374f0
Ivan Odyntsov <iwan.odintsov@gmail.com>
2020-04-11 14:53:28 +0300

    chat reputation service and job added

--- src/models/ChatMember/Jobs/reputationProcessing.js
+++ src/models/ChatMember/Jobs/reputationProcessing.js
@@ -5,6 +5,7 @@ import ChatMember from '../ChatMember';
 import ChatDailyReputation from '../../ChatDailyStatistics/Jobs/reputationProcessing';
 import ChatMembersStatsReputation from '../../ChatMembersStats/Jobs/reputationProcessing';
 import ChatMembersStatsReputationChanges from '../../ChatMembersStats/Jobs/reputationChanges';
+import ChatReputation from '../../Chat/Jobs/reputationProcessing';
 import ChatMemberReputationChanges from './reputationChanges';
 
 const reputationQueue = new Queue('app_reputation', queues);
@@ -27,6 +28,7 @@ reputationQueue.process(NAME, 5, async function (job) {
   });
 
   ChatMemberReputationChanges.addToQueue({ reputation, message });
+  ChatReputation.addToQueue({ reputation, message });
   ChatDailyReputation.addToQueue({ reputation, message });
   ChatMembersStatsReputation.addToQueue({ reputation, message });
   ChatMembersStatsReputationChanges.addToQueue({ reputation, message });
